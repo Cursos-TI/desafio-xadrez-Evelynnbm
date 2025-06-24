8f96260602c2771b843db40d824dcc192681e204
Evelynnbm <evelynbonafemachado@gmail.com>
2025-06-23 22:34:12 -0300

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,31 +2,44 @@
 
 int main()
 {
+    
     //movimentação rainha
-    int quantidadeMovimento = 0;
+    int movimentoTotal = 3; //determina quantas vezes será movido
+    int movimentoAtual = 0; // mantém o rastreio de quantas vezes rodou
     printf("Movimentação da rainha\n");
    do{
        //printa a cada movimento
-    printf("Rainha para esquerda\n");
-    quantidadeMovimento++;
+        printf("Cima\n");
+        movimentoAtual++;
     //valor a se rodar o código
-   }while(quantidadeMovimento < 8);
+   }while(movimentoAtual < movimentoTotal); 
    
    
    //movimento torre
    printf("Movimentação da torre\n");
-   quantidadeMovimento = 0;
-   while(quantidadeMovimento <=5){
+   movimentoAtual = 0;
+   movimentoTotal = 5;
+   while(movimentoAtual <=movimentoTotal){
        printf("Direita\n");
-       quantidadeMovimento++;
+       movimentoAtual++;
+   }
+   //movimentação do bispo
+   movimentoTotal = 2;
+   printf("Movimentação do bispo\n");
+   for (int i = 1; i <= movimentoTotal; i++) {
+       printf("Cima Direita\n");
    }
    
-   
-   //movimentação do bispo
-   printf("Movimentação do bispo\n");
-   for (int i = 1; i <= 2; i++) {
-       printf("Cima Direita\n");
-       
+   printf("\n");
+   printf("Movimentação do cavalo\n");
+   movimentoTotal = 2;
+   for(int i = 1; i <= movimentoTotal; i++){
+       movimentoAtual = 1;
+       while(movimentoAtual <= 2){
+           printf("cima ");
+           movimentoAtual++;
+       }
+       printf("direita\n");
    }
     return 0;
 }
