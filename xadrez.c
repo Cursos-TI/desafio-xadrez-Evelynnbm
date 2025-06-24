4edd93539a6c5263ddaf0a511900c35cb40ba365
Evelynnbm <evelynbonafemachado@gmail.com>
2025-06-23 22:10:02 -0300

    Nivel novato
    atualizando os valores, direções e colocando no padrão solicitado

--- xadrez.c
+++ xadrez.c
@@ -10,23 +10,22 @@ int main()
     printf("Rainha para esquerda\n");
     quantidadeMovimento++;
     //valor a se rodar o código
-   }while(quantidadeMovimento < 5);
+   }while(quantidadeMovimento < 8);
    
    
    //movimento torre
    printf("Movimentação da torre\n");
    quantidadeMovimento = 0;
    while(quantidadeMovimento <=5){
-       printf("Torre para cima\n");
+       printf("Direita\n");
        quantidadeMovimento++;
    }
    
    
    //movimentação do bispo
    printf("Movimentação do bispo\n");
-   for (int i = 1; i <= 5; i++) {
-       printf("Bispo para cima\n");
-       printf("Bispo paradireita\n");
+   for (int i = 1; i <= 2; i++) {
+       printf("Cima Direita\n");
        
    }
     return 0;
